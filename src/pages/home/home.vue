ce34690b9df01c6022e680afe39a2b15d70b92b1
zhangying <alisa0518@qq.com>
2020-06-11 13:57:47 +0800

    首页轮播图功能逻辑

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -2,17 +2,20 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from "./comments/header";
 import HomeSwiper from "./comments/swiper";
+import HomeIcons from "./comments/icons";
 export default {
   name: "home",
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   }
 };
 </script>
