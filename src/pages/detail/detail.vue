bb9136eb61fef768a478d62924280d93fd93f1bb
zhangying <alisa0518@qq.com>
2020-06-16 17:38:57 +0800

    轮播的渐隐渐显效果

--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -1,17 +1,22 @@
 <template>
     <div>
         <detailBanner></detailBanner>
+        <detailHeader></detailHeader>
+        <div class="content"></div>
     </div>
 </template>
 <script>
 import detailBanner from './comments/banner'
+import detailHeader from './comments/header'
 export default {
     name:"detail",
     components:{
-        detailBanner
+        detailBanner,
+        detailHeader
     }
 }
 </script>
 <style lang="stylus" scoped>
-
+    .content
+        height 50rem
 </style>
